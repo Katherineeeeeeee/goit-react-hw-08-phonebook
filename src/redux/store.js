b1c8b6ce893548c751378614af15e0eac61f090a
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-09-07 12:25:19 +0300

    added redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import contactsReducer from './contacts/items/items-reducer';
 import filterReducer from './contacts/filter/filter-reducer';
 
